e8325f87838edb31cc75b8fad5ece20560964087
alfredo <alfredtito97@gmail.com>
2021-08-27 09:16:10 -0300

    feat: adicionado request para o prismic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import { GetStaticProps } from 'next';
 
+import Prismic from '@prismicio/client';
 import { getPrismicClient } from '../services/prismic';
-
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
 
@@ -24,13 +24,42 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ postsPagination }: HomeProps): JSX.Element {
+  // TODO
+  console.log(postsPagination);
+  return (
+    <>
+      <h1>Hello World</h1>
+      <span>teste</span>
+    </>
+  );
+}
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const postsResponse = await prismic.query(
+    [Prismic.predicates.at('document.type', 'post')],
+    {
+      fetch: ['post.title', 'post.subtitle', 'post.author'],
+      pageSize: 100,
+    }
+  );
 
-//   // TODO
-// };
+  const posts: Post[] = postsResponse.results.map(post => {
+    return {
+      uid: post.uid,
+      first_publication_date: post.first_publication_date,
+      data: post.data,
+    };
+  });
+
+  // TODO;
+  return {
+    props: {
+      postsPagination: {
+        next_page: postsResponse.next_page,
+        results: posts,
+      },
+    },
+  };
+};
--- src/services/prismic.ts
+++ src/services/prismic.ts
@@ -1,10 +1,10 @@
-import Prismic from '@prismicio/client';
 import { DefaultClient } from '@prismicio/client/types/client';
 
+import Prismic from '@prismicio/client';
+
 export function getPrismicClient(req?: unknown): DefaultClient {
-  const prismic = Prismic.client(process.env.PRISMIC_API_ENDPOINT, {
+  const prismic = Prismic.client('https://igniteblog923487.prismic.io/api/v2', {
     req,
   });
-
   return prismic;
 }
